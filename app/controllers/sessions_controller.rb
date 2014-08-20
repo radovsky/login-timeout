1dcb0ad9344388ced4fc30290ba5a13f76e7d47c
Gabe Radovsky <gabriel.radovsky@gmail.com>
2014-08-20 16:09:07 -0700

    add methods to SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+  def create
+    user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+    if user
+      sign_in!(user)
+      redirect_to root_url
+    else
+      flash[:errors] = ["Username and/or password is invalid."]
+      redirect_to new_user_url
+    end
+  end
+  
+  def destroy
+    sign_out!
+    redirect_to new_user_url
+  end
 end
