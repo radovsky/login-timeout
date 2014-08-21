16a619e4d343e0622ea92c356ae6ef3920e30d3d
Gabe Radovsky <gabriel.radovsky@gmail.com>
2014-08-20 23:50:42 -0700

    set failed login attempts and time on sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,12 @@ class SessionsController < ApplicationController
       params[:user][:password]
     )
     if user
+      user.failed_login_attempts = 0
       sign_in!(user)
       redirect_to root_url
     else
+      user.failed_login_attempts += 1
+      user.failed_login_time = Time.now
       flash[:errors] = ["Username and/or password is invalid."]
       redirect_to new_user_url
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      @user.failed_login_attempts = 0
       sign_in!(@user)
       redirect_to root_url
     else
